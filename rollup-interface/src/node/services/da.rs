438dfaeec6fb6f0158478b078d5218e451d5ab07
kobakaku <kobakaku1616@gmail.com>
2024-04-08 17:06:17 +0200

    feat: setup DASpec

--- rollup-interface/src/node/services/da.rs
+++ rollup-interface/src/node/services/da.rs
@@ -1,5 +1,30 @@
-use crate::state::da::DaSpec;
+use crate::state::da::{DaSpec, DaVerifier};
 
+/// The DaService is the local side of an RPC connection talking to a node of the DA layer.
+/// The DaService has two responsibilities - facing data from the DA layer, transforming the
+/// data into a representation that can be efficiently verified in circuit.
 pub trait DaService {
+    /// A handle to the types used by the DA layer.
     type Spec: DaSpec;
+
+    /// The verifier for the DA layer.
+    type Verifier: DaVerifier;
+
+    /// The DA lauyer block.
+    type Block: SlotData;
+
+    /// Fetch the block at the given height.
+    fn get_block_at(&self, height: u64) -> anyhow::Result<Self::Block>;
+
+    /// Fetch the BlockHeader of the last finalized block.
+    fn get_block_header(&self) -> anyhow::Result<<Self::Spec as DaSpec>::BlockHeader>;
+
+    /// Send a transaction directly to the DA layer.
+    /// Returns nothing if the transaction wa successfully sent.
+    fn send_transaction(&self, blob: &[u8]) -> anyhow::Result<()>;
+}
+
+/// `SlotData` is the subset of the DA layer block which is stored in the rollup's database.
+pub trait SlotData {
+    type BlockHeader;
 }
--- rollup-interface/src/state/da.rs
+++ rollup-interface/src/state/da.rs
@@ -1,3 +1,22 @@
+/// A specification for the types used by the DA layer.
 pub trait DaSpec {
+    /// The hash of the DA layer block.
+    type BlockHash;
+
+    /// The block header type used by the DA layer.
     type BlockHeader;
+
+    /// The transaction used by the DA layer.
+    type BlobTransaction;
+
+    type Address;
+}
+
+/// A `DaVerifier` implements the logic required to create a zk proof that some data has been processed.
+pub trait DaVerifier {
+    type Spec: DaSpec;
+
+    fn new() -> Self;
+
+    fn verify() -> anyhow::Result<()>;
 }
